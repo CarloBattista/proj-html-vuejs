3223480d54172fa1121ac2d354e9c3e7ec67a5d0
CarloBattista <carlitobatti@gmail.com>
2023-05-12 17:18:09 +0200

    sezione immagine quickstart section

--- src/components/QuickstartComp.vue
+++ src/components/QuickstartComp.vue
@@ -7,7 +7,7 @@
                 <p class="descriptionSection">Start working with <span class="highlight">Landrick</span> that can provide you need to generate awareness, drive traffic, connect.</p>
             </div>
             <div class="container_image">
-                <div class="image_quickstart"></div>
+                <img class="image_quickstart" src="/_resources/_imgs/quickstart-npm.jpg" alt="quickstart">
             </div>
             <div class="container_check">
                 <div class="check_container">
@@ -32,5 +32,38 @@ export default {
 <style scoped>
 .quickstart_container{
     width: 100%;
+}
+
+.titleSection{
+    color: #2F55D4;
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 22px;
+}
+
+.headingSection{
+    color: #161C2D;
+    font-size: 2rem;
+    font-weight: 700;
+    margin-bottom: 22px;
+}
+
+.descriptionSection{
+    color: #8492A6;
+    font-size: 1rem;
+    font-weight: 500;
+    width: 55%;
+    margin: 0 auto;
+}
+
+.container_image{
+    margin-top: 60px;
+    width: 100%;
+}
+
+.image_quickstart{
+    margin: 0 auto;
+    border-radius: 6px;
+    box-shadow: 0 0 3px #000;
 }
 </style>
