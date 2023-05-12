f6947825b1a7e6f72548324477c88b0dd776675b
CarloBattista <carlitobatti@gmail.com>
2023-05-12 14:10:55 +0200

    creazione componente header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <script>
+import HeaderComp from './components/HeaderComp.vue';
 
+export default {
+  name: "App",
+  components: {
+    HeaderComp
+  }
+}
 </script>
 
 <template>
-
+  <HeaderComp />
 </template>
 
 <style>
+.w-60{
+  width: 60%;
+  margin: 0 auto;
+}
 
+.d-flex{
+  display: flex;
+}
+
+.align-items-center{
+  align-items: center;
+}
+
+.justify-c-center{
+  justify-content: center;
+}
+
+.justify-c-between{
+  justify-content: space-between;
+}
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+        <div class="w-60 d-flex align-items-center justify-c-between">
+            <div class="region_left">
+                <a href="/">
+                    <img class="logoSite" src="/_resources/_imgs/logo-dark.png" alt="logo site landrick dark">
+                </a>
+            </div>
+            <div class="region_center"></div>
+            <div class="region_right"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComp"
+}
+</script>
+
+<style scoped>
+.header {
+    position: sticky;
+    z-index: 999;
+    width: 100%;
+    height: 80px;
+    display: flex;
+    align-items: center;
+}
+
+.logoSite{
+    width: 130px;
+}
+</style>
